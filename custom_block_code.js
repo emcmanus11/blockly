afb2f26118e005454333f9834463bb22a09e8537
emcmanus11 <elyse_mcmanus@brown.edu>
2014-12-06 14:14:52 -0500

    search term shows up in input bar

--- custom_block_code.js
+++ custom_block_code.js
@@ -3,11 +3,11 @@
 // site block
 Blockly.Blocks['search_site'] = {
 	init: function() {
-	    this.setHelpUrl('http://www.example.com/');
-	    this.appendValueInput("NAME")
+	    //this.setHelpUrl('http://www.example.com/');
+	    this.appendValueInput("NAME2")
 	        .setCheck("String")
 	        .appendField("search within site")
-	        .appendField(new Blockly.FieldTextInput("site name"), "NAME");
+	        .appendField(new Blockly.FieldTextInput("site name"), "NAME2");
 	    this.setOutput(true);
 	    this.setTooltip('');
 	}
@@ -16,11 +16,11 @@ Blockly.Blocks['search_site'] = {
 // exact phrase block
 Blockly.Blocks['search_exact'] = {
 	init: function() {
-		this.setHelpUrl('http://www.example.com/');
-		this.appendValueInput("NAME")
+		//this.setHelpUrl('http://www.example.com/');
+		this.appendValueInput("NAME3")
 		    .setCheck("String")
 		    .appendField("search exact phrase")
-		    .appendField(new Blockly.FieldTextInput("ex. vegan desserts"), "NAME");
+		    .appendField(new Blockly.FieldTextInput("ex. cat paw"), "NAME3");
 		this.setOutput(true);
 		this.setTooltip('');
 	}
@@ -29,11 +29,11 @@ Blockly.Blocks['search_exact'] = {
 // do not include 
 Blockly.Blocks['do_not_include'] = {
 	init: function() {
-		this.setHelpUrl('http://www.example.com/');
-		this.appendValueInput("NAME")
+		//this.setHelpUrl('http://www.example.com/');
+		this.appendValueInput("NAME4")
 		    .setCheck("String")
 		    .appendField("do not include")
-		    .appendField(new Blockly.FieldTextInput("ex. vegetarian"), "NAME");
+		    .appendField(new Blockly.FieldTextInput("ex. kitten"), "NAME4");
 		this.setOutput(true);
 		this.setTooltip('');
 	}
@@ -42,13 +42,13 @@ Blockly.Blocks['do_not_include'] = {
 //within range
 Blockly.Blocks['within_this_range'] = {
 	init: function() {
-		this.setHelpUrl('http://www.example.com/');
+		//this.setHelpUrl('http://www.example.com/');
 		this.appendValueInput("NAME")
 		    .setCheck("String")
 		    .appendField("range from")
 		    .appendField(new Blockly.FieldTextInput("ex. $10, etc. "), "NAME")
 		    .appendField("to")
-		    .appendField(new Blockly.FieldTextInput("ex. 2$0, etc. "), "NAME1");
+		    .appendField(new Blockly.FieldTextInput("ex. $20, etc. "), "NAME1");
 		this.setOutput(true);
 		this.setTooltip('');
 	}
@@ -58,8 +58,8 @@ Blockly.Blocks['within_this_range'] = {
 //wildcard
 Blockly.Blocks['wildcard'] = {
 	init: function() {
-		this.setHelpUrl('http://www.example.com/');
-		this.appendValueInput("NAME")
+		//this.setHelpUrl('http://www.example.com/');
+		this.appendValueInput("NAME5")
 		    .appendField("wildcard");
 		this.setOutput(true);
 		this.setTooltip('');
@@ -69,8 +69,8 @@ Blockly.Blocks['wildcard'] = {
 // OR
 Blockly.Blocks['or'] = {
 	init: function() {
-		this.setHelpUrl('http://www.example.com/');
-		this.appendValueInput("NAME")
+		//this.setHelpUrl('http://www.example.com/');
+		this.appendValueInput("NAME6")
 		    .appendField("OR");
 		this.setOutput(true);
 		this.setTooltip('');
@@ -83,69 +83,73 @@ Blockly.Blocks['or'] = {
 
 // site block
 Blockly.JavaScript['search_site'] = function(block) {
-	var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-	var text_name = block.getFieldValue('NAME');
+	var value_name = Blockly.JavaScript.valueToCode(block, 'NAME2', Blockly.JavaScript.ORDER_ATOMIC);
+	var text_name = block.getFieldValue('NAME2');
 	// TODO: Assemble JavaScript into code variable.
-	var code = '...';
+	var code = "site:" + text_name + " " + value_name;
 	// TODO: Change ORDER_NONE to the correct strength.
-	return [code, Blockly.JavaScript.ORDER_NONE];
+	return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
 // exact phrase block
 Blockly.JavaScript['search_exact'] = function(block) {
-	var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-	var text_name = block.getFieldValue('NAME');
+	var value_name = Blockly.JavaScript.valueToCode(block, 'NAME3', Blockly.JavaScript.ORDER_ATOMIC);
+	var text_name = block.getFieldValue('NAME3');
 	// TODO: Assemble JavaScript into code variable.
-	var code = '...';
+	var code = '"' + text_name + '" ' + value_name;
 	// TODO: Change ORDER_NONE to the correct strength.
-	return [code, Blockly.JavaScript.ORDER_NONE];
+	return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
 // do not include
 Blockly.JavaScript['do_not_include'] = function(block) {
-	var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-	var text_name = block.getFieldValue('NAME');
+	var value_name = Blockly.JavaScript.valueToCode(block, 'NAME4', Blockly.JavaScript.ORDER_ATOMIC);
+	var text_name = block.getFieldValue('NAME4');
 	// TODO: Assemble JavaScript into code variable.
-	var code = '...';
+	var code = '-' + text_name + " " + value_name;
 	// TODO: Change ORDER_NONE to the correct strength.
-	return [code, Blockly.JavaScript.ORDER_NONE];
+	return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
 //within range
 Blockly.JavaScript['within_this_range'] = function(block) {
 	var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
 	var text_name = block.getFieldValue('NAME');
+	var value_name1 = Blockly.JavaScript.valueToCode(block, 'NAME1', Blockly.JavaScript.ORDER_ATOMIC);
 	var text_name1 = block.getFieldValue('NAME1');
 	// TODO: Assemble JavaScript into code variable.
-	var code = '...';
+	var code = text_name + '..' + text_name1 + " " + value_name;
 	// TODO: Change ORDER_NONE to the correct strength.
-	return [code, Blockly.JavaScript.ORDER_NONE];
+	return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
 //wildcard
 Blockly.JavaScript['wildcard'] = function(block) {
-	var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
+	var value_name = Blockly.JavaScript.valueToCode(block, 'NAME5', Blockly.JavaScript.ORDER_ATOMIC);
 	// TODO: Assemble JavaScript into code variable.
-	var code = '...';
+	var code = ' * ' + value_name;
 	// TODO: Change ORDER_NONE to the correct strength.
-	return [code, Blockly.JavaScript.ORDER_NONE];
+	return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
 //or
 Blockly.JavaScript['or'] = function(block) {
-	var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
+	var value_name = Blockly.JavaScript.valueToCode(block, 'NAME6', Blockly.JavaScript.ORDER_ATOMIC);
 	// TODO: Assemble JavaScript into code variable.
-	var code = '...';
+	var code = ' OR ' + value_name;
 	// TODO: Change ORDER_NONE to the correct strength.
-	return [code, Blockly.JavaScript.ORDER_NONE];
+	return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
-/*
 function myUpdateFunction() {
+	console.log("%%%%%%%%%%%%%%%%%%%%%%%")
 	var code = Blockly.JavaScript.workspaceToCode();
+	code = code.substring(0, code.length - 3)
 	console.log("code");
-	console.log(code);
-	// document.getElementById('textarea').value = code;
+	console.log(code.search("\n") > -1);
+	if (code.search("\n") <= -1) {
+		var textarea = document.getElementById('searchText'),
+		currText = textarea.value
+		textarea.value = code;
+	}
 }
-Blockly.addChangeListener(myUpdateFunction);
-*/
